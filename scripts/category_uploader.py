e2453d8043a1562fe7fb9650e86e79a8753f80c2
Sadia Zahin Diya <sadia.zahin@tigerit.com>
2020-04-26 15:42:28 +0600

    Add script for category upload

---
+++ scripts/category_uploader.py
@@ -0,0 +1,56 @@
+import logging
+import re
+from logging.handlers import TimedRotatingFileHandler
+
+import pandas as pd
+import numpy as np
+import requests
+
+logger = logging.getLogger('category uploader logger')
+logger.setLevel(logging.DEBUG)
+handler = TimedRotatingFileHandler('category_uploader.log', when='midnight', interval=1,  backupCount=30)
+handler.setLevel(logging.DEBUG)
+handler.setFormatter(logging.Formatter(
+    fmt='[%(asctime)s.%(msecs)03d] [%(levelname)s]: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+))
+handler.suffix = "%Y%m%d"
+handler.extMatch = re.compile(r"^\d{8}$")
+logger.addHandler(handler)
+
+rs = requests.session()
+_http_headers = {'Content-Type': 'application/json'}
+
+ES_HOST = 'localhost:9200'
+
+
+def add_category(data):
+    print("Call service with data " , data)
+
+
+def category_extract():
+    data = pd.read_csv("../datasets/category-list - algorithms.csv")
+    data = data.replace({np.nan: None})
+    for i in range(0, len(data)):
+        if data['category_root'][i]:
+            category_root = data['category_root'][i]
+        json_data = {
+            "category_name": data['category_name'][i],
+            "category_root": category_root,
+            "category_difficulty": data['category_difficulty'][i],
+            "category_importance": data['category_importance'][i]
+        }
+        add_category(json_data)
+
+    print(len(data))
+    print()
+
+
+category_extract()
+
+
+
+
+
+
+
