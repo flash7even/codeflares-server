4fc2eacfdec62a5daa77f87c104f148f290d51d9
tarangokhan <tarangokhan77@gmailcom>
2020-04-29 17:47:34 +0600

    Update spoj scrapper models

--- scrappers/spoj_scrapper.py
+++ scrappers/spoj_scrapper.py
@@ -7,29 +7,32 @@ import re
 _http_headers = {'Content-Type': 'application/json'}
 
 
-def get_user_info(username):
-    rs = requests.session()
-    url = f'http://www.spoj.com/users/{username}'
-    profile_page = rs.get(url=url, headers=_http_headers)
-    soup = BeautifulSoup(profile_page.text, 'html.parser')
+class SpojScrapper:
 
-    problems = []
-    contentTable = soup.find('table', {"class": "table-condensed"})
+    def get_user_info(self, username):
+        rs = requests.session()
+        url = f'http://www.spoj.com/users/{username}'
+        profile_page = rs.get(url=url, headers=_http_headers)
+        soup = BeautifulSoup(profile_page.text, 'html.parser')
 
-    for link in contentTable.findAll('a'):
-        problem_name = link.string
-        if problem_name is not None:
-            problems.append(problem_name)
+        problems = []
+        contentTable = soup.find('table', {"class": "table-condensed"})
 
-    return {
-        'platform': 'spoj',
-        'user_name': username,
-        'solved_count': len(problems),
-        'solved_problems': problems
-    }
+        for link in contentTable.findAll('a'):
+            problem_name = link.string
+            if problem_name is not None:
+                problems.append(problem_name)
+
+        return {
+            'platform': 'spoj',
+            'user_name': username,
+            'solved_count': len(problems),
+            'solved_problems': problems
+        }
 
 
 if __name__ == '__main__':
     print('START RUNNING SPOJ SCRAPPER SCRIPT\n')
-    resp = get_user_info('tarango_khan')
+    spoj_scrapper = SpojScrapper()
+    resp = spoj_scrapper.get_user_info('tarango_khan')
     print(json.dumps(resp))
