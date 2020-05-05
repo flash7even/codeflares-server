472afa8a769ccbf98e4e6ba6de26c42c5b07c5e2
Tarango Khan <tarangokhan77@gmail.com>
2020-05-05 21:44:16 +0600

    Update difficulty generation logic

--- scripts/light_oj_problem_downloader.py
+++ scripts/light_oj_problem_downloader.py
@@ -1,5 +1,16 @@
 import csv
 
+header = [
+    'problem_name',
+    'problem_id',
+    'problem_difficulty',
+    'problem_significance',
+    'oj_name',
+    'source_link',
+    'solved_count',
+    'tried_count',
+]
+
 
 def download_in_csv_format(file_name, data):
     with open(file_name, 'w+') as my_csv:
@@ -7,35 +18,19 @@ def download_in_csv_format(file_name, data):
         csvWriter.writerows(data)
 
 
-data_list = []
-
-
-def make_table_header():
-    header = [
-        'problem_name',
-        'problem_id',
-        'problem_difficulty',
-        'problem_difficulty_org',
-        'problem_significance',
-        'oj_name',
-        'source_link',
-        'solved_count',
-        'tried_count',
-    ]
+def make_table_rows(file_source, file_dest, min_diff, max_diff):
+    data_list = []
     data_list.append(header)
+    problem_count = 0
 
+    with open(file_source, mode='r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            problem_count += 1
 
-def make_table_rows(file_source):
-
-    max_solved_value = 1525
-    max_tried_value = 1550
-
-    scale_top = 1.0
-    scale_top_dx = 0.02
-    top_dif = 3.25
-
-    scale_below = 1.0
-    scale_below_dx = 0.003295
+    scale = (max_diff - min_diff)/problem_count
+    cur_diff = min_diff
+    json_list = []
 
     with open(file_source, mode='r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
@@ -48,37 +43,49 @@ def make_table_rows(file_source):
             }
             solved_tried = row["solved"].replace(" ", "")
             mlist = solved_tried.split("/")
+            d['solved_count'] = int(mlist[0])
+            d['tried_count'] = mlist[1]
+            json_list.append(d)
 
-            if int(mlist[0]) > max_solved_value:
-                d['solved_count'] = mlist[0]
-                d['tried_count'] = mlist[1]
-                d['problem_difficulty'] = top_dif*scale_top
-                d['problem_difficulty_org'] = (10.0 - 6.50*float(mlist[0])/float(max_solved_value))
-                d['problem_significance'] = 8.25
-                scale_top += scale_top_dx
-            else:
-                d['solved_count'] = mlist[0]
-                d['tried_count'] = mlist[1]
-                d['problem_difficulty'] = (10.0 - 6.50*float(mlist[0])/float(max_solved_value)) * scale_below
-                d['problem_difficulty_org'] = (10.0 - 6.75*float(mlist[0])/float(max_solved_value))
-                d['problem_significance'] = 9.5*float(mlist[1])/float(max_tried_value)
-                scale_below -= scale_below_dx
-                print(scale_below)
+    json_list = sorted(json_list, key=lambda i: i['solved_count'])
+    json_list.reverse()
 
-            d['problem_difficulty'] = "{:.2f}".format(d['problem_difficulty'])
-            d['problem_significance'] = "{:.2f}".format(d['problem_significance'])
+    for d in json_list:
+        d['problem_difficulty'] = cur_diff
+        d['problem_significance'] = (10.0 - cur_diff)
+        d['problem_difficulty'] = "{:.2f}".format(d['problem_difficulty'])
+        d['problem_significance'] = "{:.2f}".format(d['problem_significance'])
 
-            data = [d['problem_name'], d['problem_id'], d['problem_difficulty'], d['problem_significance'],
-                    d['oj_name'],d['source_link'], d['solved_count'], d['tried_count']]
+        data = [d['problem_name'], d['problem_id'], d['problem_difficulty'], d['problem_significance'],
+                d['oj_name'],d['source_link'], d['solved_count'], d['tried_count']]
+        data_list.append(data)
+        cur_diff += scale
 
-            #print(data)
-            data_list.append(data)
+    download_in_csv_format(file_dest, data_list)
 
 
 if __name__ == '__main__':
-    file_source = 'Light OJ - DP - Sheet1.csv'
-    make_table_header()
-    make_table_rows(file_source)
-    download_in_csv_format('light_oj_dp.csv', data_list)
+
+    source_file_list = ['lightoj-basic-math - Sheet1.csv', 'light-oj-beginners - Sheet1.csv', 'light-oj-bfs-dfs - Sheet1.csv', 'lightoj-binary-search - Sheet1.csv',
+                 'lightoj-bpm - Sheet1.csv', 'lightoj-counting - Sheet1.csv', 'lightoj-dijkstra - Sheet1.csv', 'lightoj-dp - Sheet1.csv'
+                 , 'lightoj-greedy - Sheet1.csv', 'lightoj-matrix-exponentiation - Sheet1.csv', 'lightoj-maxflow - Sheet1.csv', 'lightoj-mcmf - Sheet1.csv',
+                 'lightoj-mst - Sheet1.csv', 'lightoj-number-theory - Sheet1.csv', 'lightoj-segment-tree - Sheet1.csv', 'lightoj-trie - Sheet1.csv']
+
+    min_difficulty = [2.5, 1.5, 3.00, 2.75, 5.0, 3.0, 4.65, 3.25, 2.25, 4.5, 4.60, 5.50, 4.15, 2.50, 4.50, 3.50]
+    max_difficulty = [4.5, 3.5, 6.25, 6.25, 8.5, 7.75, 7.85, 9.00, 6.5, 8.0, 9.00, 9.00, 5.75, 8.55, 8.50, 6.55]
+
+    file_count = len(source_file_list)
+
+    for idx in range(0, file_count):
+        file_name = source_file_list[idx]
+        file_name = file_name.replace(" ", "")
+        words = file_name.split("-")
+        words = words[:-1]
+        file_dest = "_".join(words)
+        file_dest += ".csv"
+        src = "../dataset-row/lightoj-row/" + source_file_list[idx]
+        dst = "../dataset-row/lightoj/" + file_dest
+        make_table_rows(src, dst, min_difficulty[idx], max_difficulty[idx])
+
 
 
