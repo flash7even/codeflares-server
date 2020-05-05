d9dde9fd94576ec49a64f523c35833085df0499e
tarangokhan <tarangokhan77@gmailcom>
2020-05-05 17:00:39 +0600

    Update scale value in lighoj problem script

--- scripts/light_oj_problem_downloader.py
+++ scripts/light_oj_problem_downloader.py
@@ -15,6 +15,7 @@ def make_table_header():
         'problem_name',
         'problem_id',
         'problem_difficulty',
+        'problem_difficulty_org',
         'problem_significance',
         'oj_name',
         'source_link',
@@ -26,22 +27,15 @@ def make_table_header():
 
 def make_table_rows(file_source):
 
-    max_solved_value = 0
-    max_tried_value = 0
+    max_solved_value = 1525
+    max_tried_value = 1550
 
-    with open(file_source, mode='r') as csv_file:
-        csv_reader = csv.DictReader(csv_file)
+    scale_top = 1.0
+    scale_top_dx = 0.02
+    top_dif = 3.25
 
-        for row in csv_reader:
-            solved_tried = row["solved"].replace(" ", "")
-            mlist = solved_tried.split("/")
-            solved_val = int(mlist[0])
-            tried_val = int(mlist[1])
-            max_solved_value = max(max_solved_value, solved_val)
-            max_tried_value = max(max_tried_value, tried_val)
-
-        print('max_solved_value', max_solved_value)
-        print('max_tried_value', max_tried_value)
+    scale_below = 1.0
+    scale_below_dx = 0.003295
 
     with open(file_source, mode='r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
@@ -54,17 +48,30 @@ def make_table_rows(file_source):
             }
             solved_tried = row["solved"].replace(" ", "")
             mlist = solved_tried.split("/")
-            d['solved_count'] = mlist[0]
-            d['tried_count'] = mlist[1]
-            d['problem_difficulty'] = (10.0 - 6.75*float(mlist[0])/float(max_solved_value))
+
+            if int(mlist[0]) > max_solved_value:
+                d['solved_count'] = mlist[0]
+                d['tried_count'] = mlist[1]
+                d['problem_difficulty'] = top_dif*scale_top
+                d['problem_difficulty_org'] = (10.0 - 6.50*float(mlist[0])/float(max_solved_value))
+                d['problem_significance'] = 8.25
+                scale_top += scale_top_dx
+            else:
+                d['solved_count'] = mlist[0]
+                d['tried_count'] = mlist[1]
+                d['problem_difficulty'] = (10.0 - 6.50*float(mlist[0])/float(max_solved_value)) * scale_below
+                d['problem_difficulty_org'] = (10.0 - 6.75*float(mlist[0])/float(max_solved_value))
+                d['problem_significance'] = 9.5*float(mlist[1])/float(max_tried_value)
+                scale_below -= scale_below_dx
+                print(scale_below)
+
             d['problem_difficulty'] = "{:.2f}".format(d['problem_difficulty'])
-            d['problem_significance'] = 9.5*float(mlist[1])/float(max_tried_value)
             d['problem_significance'] = "{:.2f}".format(d['problem_significance'])
 
             data = [d['problem_name'], d['problem_id'], d['problem_difficulty'], d['problem_significance'],
                     d['oj_name'],d['source_link'], d['solved_count'], d['tried_count']]
 
-            print(data)
+            #print(data)
             data_list.append(data)
 
 
