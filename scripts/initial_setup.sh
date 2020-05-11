275a19dd74b6dc022d754be9d6d06931295185b0
Tarango Khan <tarangokhan77@gmail.com>
2020-05-11 23:13:40 +0600

    Add bash script to run all the scripts during initialization

---
+++ scripts/initial_setup.sh
@@ -0,0 +1,6 @@
+python schema_uploader.py --dir ../schema --delete true --create true
+python admin_uploader.py
+python contestant_uploader.py
+python category_uploader.py
+python category_dependency_uploader.py
+python problem_list_uploader.py
