17b42e27a873cc26ab2a1c55cf7114eaecaaca48
tarangokhan <tarangokhan77@gmailcom>
2020-05-13 14:28:31 +0600

    Add classroom class controller

---
+++ apis/classroom_class_controller.py
@@ -0,0 +1,206 @@
+import time
+
+import requests
+from flask import current_app as app
+from flask import request
+from flask_restplus import Namespace, Resource
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended.exceptions import *
+from flask_jwt_extended import jwt_required
+from jwt.exceptions import *
+from commons.jwt_helpers import access_required
+
+api = Namespace('classroom_class', description='Namespace for classroom_class service')
+
+_http_headers = {'Content-Type': 'application/json'}
+
+_es_index_classroom_classes = 'cp_training_classroom_classes'
+_es_type = '_doc'
+_es_size = 100
+
+
+@api.errorhandler(NoAuthorizationError)
+def handle_auth_error(e):
+    return {'message': str(e)}, 401
+
+
+@api.errorhandler(CSRFError)
+def handle_auth_error(e):
+    return {'message': str(e)}, 401
+
+
+@api.errorhandler(ExpiredSignatureError)
+def handle_expired_error(e):
+    return {'message': 'Token has expired'}, 401
+
+
+@api.errorhandler(InvalidHeaderError)
+def handle_invalid_header_error(e):
+    return {'message': str(e)}, 422
+
+
+@api.errorhandler(InvalidTokenError)
+def handle_invalid_token_error(e):
+    return {'message': str(e)}, 422
+
+
+@api.errorhandler(JWTDecodeError)
+def handle_jwt_decode_error(e):
+    return {'message': str(e)}, 422
+
+
+@api.errorhandler(WrongTokenError)
+def handle_wrong_token_error(e):
+    return {'message': str(e)}, 422
+
+
+@api.errorhandler(RevokedTokenError)
+def handle_revoked_token_error(e):
+    return {'message': 'Token has been revoked'}, 401
+
+
+@api.errorhandler(FreshTokenRequired)
+def handle_fresh_token_required(e):
+    return {'message': 'Fresh token required'}, 401
+
+
+@api.errorhandler(UserLoadError)
+def handler_user_load_error(e):
+    identity = get_jwt_identity().get('id')
+    return {'message': "Error loading the user {}".format(identity)}, 401
+
+
+@api.errorhandler(UserClaimsVerificationError)
+def handle_failed_user_claims_verification(e):
+    return {'message': 'User claims verification failed'}, 400
+
+
+@api.route('/<string:classroom_class_id>')
+class ClassroomClassByID(Resource):
+
+    @api.doc('get classroom_class details by id')
+    def get(self, classroom_class_id):
+        app.logger.info('Get classroom_class_details method called')
+        rs = requests.session()
+        search_url = 'http://{}/{}/{}/{}'.format(app.config['ES_HOST'], _es_index_classroom_classes, _es_type, classroom_class_id)
+        response = rs.get(url=search_url, headers=_http_headers).json()
+        print(response)
+        if 'found' in response:
+            if response['found']:
+                data = response['_source']
+                data['id'] = response['_id']
+                app.logger.info('Get classroom_class_details method completed')
+                return data, 200
+            app.logger.warning('classroom_class not found')
+            return {'found': response['found']}, 404
+        app.logger.error('Elasticsearch down, response: ' + str(response))
+        return response, 500
+
+    @access_required(access="ALL")
+    @api.doc('update classroom_class by id')
+    def put(self, classroom_class_id):
+        app.logger.info('Update classroom_class_details method called')
+        rs = requests.session()
+        post_data = request.get_json()
+
+        search_url = 'http://{}/{}/{}/{}'.format(app.config['ES_HOST'], _es_index_classroom_classes, _es_type, classroom_class_id)
+        response = rs.get(url=search_url, headers=_http_headers).json()
+        print(response)
+        if 'found' in response:
+            if response['found']:
+                data = response['_source']
+                for key, value in post_data.items():
+                    data[key] = value
+                data['updated_at'] = int(time.time())
+                response = rs.put(url=search_url, json=data, headers=_http_headers).json()
+                if 'result' in response:
+                    app.logger.info('Update classroom_class_details method completed')
+                    return response['result'], 200
+                else:
+                    app.logger.error('Elasticsearch down, response: ' + str(response))
+                    return response, 500
+            app.logger.warning('classroom_class not found')
+            return {'message': 'not found'}, 404
+        app.logger.error('Elasticsearch down, response: ' + str(response))
+        return response, 500
+
+    @access_required(access="ALL")
+    @api.doc('delete classroom_class by id')
+    def delete(self, classroom_class_id):
+        app.logger.info('Delete classroom_class_details method called')
+        rs = requests.session()
+        search_url = 'http://{}/{}/{}/{}'.format(app.config['ES_HOST'], _es_index_classroom_classes, _es_type, classroom_class_id)
+        response = rs.delete(url=search_url, headers=_http_headers).json()
+        print(response)
+        if 'result' in response:
+            if response['result'] == 'deleted':
+                app.logger.info('Delete classroom_class_details method completed')
+                return response['result'], 200
+            else:
+                return response['result'], 400
+        app.logger.error('Elasticsearch down, response: ' + str(response))
+        return response, 500
+
+
+@api.route('/')
+class CreateClassroomClass(Resource):
+
+    @access_required(access="ALL")
+    @api.doc('create classroom_class')
+    def post(self):
+        app.logger.info('Create classroom_class method called')
+        rs = requests.session()
+        data = request.get_json()
+
+        data['created_at'] = int(time.time())
+        data['updated_at'] = int(time.time())
+
+        post_url = 'http://{}/{}/{}'.format(app.config['ES_HOST'], _es_index_classroom_classes, _es_type)
+        response = rs.post(url=post_url, json=data, headers=_http_headers).json()
+        print(response)
+
+        if 'result' in response and response['result'] == 'created':
+            app.logger.info('Create classroom_class method completed')
+            return response['_id'], 201
+        app.logger.error('Elasticsearch down, response: ' + str(response))
+        return response, 500
+
+
+@api.route('/search', defaults={'page': 0})
+@api.route('/search/<int:page>')
+class SearchClassroomClass(Resource):
+
+    @api.doc('search classroom_class based on post parameters')
+    def post(self, page=0):
+        app.logger.info('ClassroomClass search method called')
+        rs = requests.session()
+        param = request.get_json()
+        query_json = {'query': {'match_all': {}}}
+
+        must = []
+        keyword_fields = ['classroom_class_title', 'classroom_class_root']
+
+        for f in param:
+            if f in keyword_fields:
+                must.append({'term': {f: param[f]}})
+            else:
+                must.append({'match': {f: param[f]}})
+
+        if len(must) > 0:
+            query_json = {'query': {'bool': {'must': must}}}
+
+        query_json['from'] = page*_es_size
+        query_json['size'] = _es_size
+        search_url = 'http://{}/{}/{}/_search'.format(app.config['ES_HOST'], _es_index_classroom_classes, _es_type)
+        response = rs.post(url=search_url, json=query_json, headers=_http_headers).json()
+        print(response)
+        if 'hits' in response:
+            item_list = []
+            for hit in response['hits']['hits']:
+                classroom_class = hit['_source']
+                classroom_class['id'] = hit['_id']
+                item_list.append(classroom_class)
+            app.logger.info('ClassroomClass search method completed')
+            return item_list, 200
+        app.logger.error('Elasticsearch down, response: ' + str(response))
+        return {'message': 'internal server error'}, 500
