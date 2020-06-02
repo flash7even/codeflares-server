836477310c3907cf13a05effef7064195c7b95da
tarangokhan <tarangokhan77@gmailcom>
2020-06-03 02:06:29 +0600

    Add submission history scrapper for codechef

--- scrappers/codechef_scrapper.py
+++ scrappers/codechef_scrapper.py
@@ -6,8 +6,64 @@ import re
 
 _http_headers = {'Content-Type': 'application/json'}
 
-
 class CodechefScrapper:
+
+    def get_submission_history(self, username):
+        rs = requests.session()
+        year = 2020
+        while year >= 2008:
+            url = f'https://www.codechef.com/submissions?sort_by=All&sorting_order=desc&language=All&status=15&year={year}&handle={username}&pcode=&ccode=&Submit=GO'
+            submission_page = rs.get(url=url, headers=_http_headers)
+            soup = BeautifulSoup(submission_page.text, 'html.parser')
+            soup.prettify()
+            table = soup.find("table",{"class":"dataTable"})
+            for row in table.find_all("tr")[1:]:  # skipping header row
+                cells = row.find_all("td")
+                print(cells[0].text)
+                submission_id = cells[0].text
+                submission_date = cells[1].text
+                problem_id = cells[3].find('a').text
+                verdict = 'unsuccessful'
+                verdict_data = str(cells[5])
+                if verdict_data.find('accepted') != -1 or verdict_data.find('100pts') != -1:
+                    verdict = 'accepted'
+
+                submission_data = {
+                    'submission_id': submission_id,
+                    'submission_date': submission_date,
+                    'problem_id': problem_id,
+                    'verdict': verdict,
+                }
+                print(submission_data)
+            year -= 1
+
+    def get_submission_for_problem(self, username, problem_id):
+        try:
+            rs = requests.session()
+            url = f'https://www.codechef.com/status/{problem_id},{username}'
+            submission_page = rs.get(url=url, headers=_http_headers)
+            soup = BeautifulSoup(submission_page.text, 'html.parser')
+            table = soup.find("table",{"class":"dataTable"})
+            last_ac_submission = None
+            for row in table.find_all("tr")[1:]:  # skipping header row
+                cells = row.find_all("td")
+                submission_id = cells[0].text
+                submission_date = cells[1].text
+                problem_id = problem_id
+                verdict = 'unsuccessful'
+                verdict_data = str(cells[3])
+                if verdict_data.find('accepted') != -1 or verdict_data.find('100pts') != -1:
+                    verdict = 'accepted'
+                    submission_data = {
+                        'submission_id': submission_id,
+                        'submission_date': submission_date,
+                        'problem_id': problem_id,
+                        'verdict': verdict,
+                    }
+                    last_ac_submission = submission_data
+            return last_ac_submission
+        except Exception as e:
+            return None
 
     def get_user_info(slef, username):
         try:
@@ -38,9 +94,27 @@ class CodechefScrapper:
                 'solved_problems': []
             }
 
+    def get_problem_stat(self, username):
+        stat = self.get_user_info(username)
+        print(stat)
+        solved_problems = stat['solved_problems']
+        problem_stat_list = []
+        for problem_id in solved_problems:
+            print('CHECK FOR: ', problem_id)
+            problem_stat = self.get_submission_for_problem(username, problem_id)
+            if problem_stat:
+                print(problem_stat)
+                problem_stat_list.append(problem_stat)
+            else:
+                print('Problem info could not found for: ', problem_id)
+        stat['problem_stat_list'] = problem_stat_list
+        return problem_stat_list
+
 
 if __name__ == '__main__':
     print('START RUNNING CODECHEF SCRAPPER SCRIPT\n')
     codechef_scrapper = CodechefScrapper()
-    resp = codechef_scrapper.get_user_info('tarango_khan')
-    print(json.dumps(resp))
+    #resp = codechef_scrapper.get_user_info('tarango_khan')
+    #print(json.dumps(resp))
+    codechef_stat = codechef_scrapper.get_problem_stat('tarango_khan')
+    print(codechef_stat)
