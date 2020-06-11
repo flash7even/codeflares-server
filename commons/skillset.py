26235c48cdfe1ef83ce7f062db17ce1dd3b564af
tarangokhan <tarangokhan77@gmailcom>
2020-06-11 11:04:41 +0600

    Add rating changes related score configuration

--- commons/skillset.py
+++ commons/skillset.py
@@ -4,6 +4,7 @@ class Skill:
     max_skill = 1000000
 
     skill_levels = [0, 8, 20, 35, 70, 120, 200, 320, 470, 670, 920, max_skill]
+    weekly_min_score = [1, 3, 5.82, 9.01, 16.01, 24.37, 33.87, 44.39, 55.83, 68.14, 81.24, 100]
     score_table = [0, 1, 2.82, 5.19, 8, 11.18, 14.69, 18.52, 22.62, 27, 31.62]
 
     skill_title = [
@@ -62,3 +63,11 @@ class Skill:
         skill_ext = skill - range_st
         level = max_level + skill_ext/dx
         return level
+
+    def generate_next_week_prediction(self, level):
+        u = int(level)
+        v = u + 1
+        score_dx = self.weekly_min_score[v] - self.weekly_min_score[u]
+        level_dx = level - u
+        score = self.weekly_min_score[u] + score_dx*level_dx
+        return score
