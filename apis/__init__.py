9b2adea1b165e857c44bbc7e99b780b16177033a
tarangokhan <tarangokhan77@gmailcom>
2020-07-17 04:28:49 +0600

    Fix bug with email username field

--- apis/__init__.py
+++ apis/__init__.py
@@ -76,7 +76,7 @@ def create_app(instance_name):
 
     app.config['MAIL_SERVER'] = 'smtp.gmail.com'
     app.config['MAIL_PORT'] = 465
-    app.config['MAIL_USERNAME'] = os.getenv('EMAIL_USER')
+    app.config['MAIL_USERNAME'] = os.getenv('EMAIL_USERNAME')
     app.config['MAIL_PASSWORD'] = os.getenv('EMAIL_PASSWORD')
     app.config['MAIL_USE_TLS'] = False
     app.config['MAIL_USE_SSL'] = True
